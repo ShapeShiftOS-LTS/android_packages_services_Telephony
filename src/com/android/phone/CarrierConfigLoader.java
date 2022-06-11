f28211565da13f643bf0e13546f8b63301d701f1
AshutoshSundresh <ashutoshsundresh@gmail.com>
2022-06-11 11:30:46 +0530

    Revert "Use incremental version to check for system update"
    This reverts commit b8b577c6912fbc1ccac8e449069f3954a24c92ea.

--- src/com/android/phone/CarrierConfigLoader.java
+++ src/com/android/phone/CarrierConfigLoader.java
@@ -539,16 +539,16 @@ public class CarrierConfigLoader extends ICarrierConfigLoader.Stub {
                     SharedPreferences sharedPrefs =
                             PreferenceManager.getDefaultSharedPreferences(mContext);
                     final String lastFingerprint = sharedPrefs.getString(KEY_FINGERPRINT, null);
-                    if (!Build.VERSION.INCREMENTAL.equals(lastFingerprint)) {
+                    if (!Build.FINGERPRINT.equals(lastFingerprint)) {
                         logd(
                                 "Build fingerprint changed. old: "
                                         + lastFingerprint
                                         + " new: "
-                                        + Build.VERSION.INCREMENTAL);
+                                        + Build.FINGERPRINT);
                         clearCachedConfigForPackage(null);
                         sharedPrefs
                                 .edit()
-                                .putString(KEY_FINGERPRINT, Build.VERSION.INCREMENTAL)
+                                .putString(KEY_FINGERPRINT, Build.FINGERPRINT)
                                 .apply();
                     }
                     break;
